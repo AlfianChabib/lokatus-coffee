6ba52fad3589a1a8f78ab1ec43ba5cd1059a5f5b
AlfianChabib <alfianchabib109@gmail.com>
2024-12-07 19:43:12 +0700

    update query

--- src/hooks/quotes/useGetQuote.ts
+++ src/hooks/quotes/useGetQuote.ts
@@ -10,6 +10,7 @@ export default function useGetQuote() {
     queryKey: ["quote"],
     queryFn: getQuote,
     staleTime: 0,
+    retry: false,
     refetchOnWindowFocus: false,
     refetchOnReconnect: true,
     refetchInterval: false,
